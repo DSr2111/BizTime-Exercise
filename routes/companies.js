b2edaea549de5b89b09a47b30f25501b46e6f50b
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-07-26 09:56:06 -0400

    companies :code route wip

--- routes/companies.js
+++ routes/companies.js
@@ -21,21 +21,34 @@ router.get("/", async function (req, res, next) {
 
 // get info on a company
 router.get("/:code", async function (req, res, next) {
-  let code = req.params.code;
+    try {
+      let code = req.params.code;
+  
+      const compResult = await db.query(
+            `SELECT code, name, description
+             FROM companies
+             WHERE code = $1`,
+          [code]
+      );
+  
+      const invResult = await db.query(
+            `SELECT id
+             FROM invoices
+             WHERE comp_code = $1`,
+          [code]
+      );
 
-  const compResult = await db.query(
-    `SELECT code, name, description
-        FROM companies
-        WHERE code = $1`,
-    [code]
-  );
-  const invResult = await db.query(
-    `
-        SELECT id
-        FROM invoices
-        WHERE comp_code = $1`,
-    [code]
-  );
+      if (compResult.rows.length === 0) {
+        throw new ExpressError(`No matching company: ${code}`, 404)
+      }
+
+      const company = compResult.rows=[0];
+      const invoice = invResult.rows;
+
+      company.invoices = invoices.map(inv => inv.id);
+
+      return res.json({"company": company});
+    }
 });
 router.post("/", async function (req, res, next) {});
 router.put("/", async function (req, res, next) {});
