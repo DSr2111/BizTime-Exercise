9fb9965c368a3cc720d3cd91f7a0078737b80f20
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-07-26 08:10:35 -0400

    router start

--- routes/companies.js
+++ routes/companies.js
@@ -3,3 +3,6 @@ const slugify = require("slugify");
 const ExpressError = require("../expressError");
 const db = require("../db");
 
+let router = new express.router();
+
+router.get("/", async function (req, res, next) {});
