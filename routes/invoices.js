af7c3a337cdb2b4e0123b162870a0807884cd7f5
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-07-30 06:18:52 -0400

    specific invoice route

--- routes/invoices.js
+++ routes/invoices.js
@@ -17,9 +17,26 @@ router.get("/", async function (req, res, next) {
   }
 });
 
-router.get("/", async function (req, res, next) {
+router.get("/:id", async function (req, res, next) {
   try {
-  } catch (err) {}
+    let id = req.params.id;
+
+    const result = await db.query(
+      `SELECT i.id,
+        i.amt,
+        i.paid,
+        i.add_date,
+        i.paid_date
+        c.name,
+        c.description
+        FROM invoices AS i
+        INNER JOIN companies AS c ON (i.comp_code = c.code)
+        WHERE id = $1`,
+      [id]
+    );
+  } catch (err) {
+    throw new ExpressError(`No such invoice ${id}`, 404);
+  }
 });
 
 router.post("/", async function (req, res, next) {
