70f05b69043d62ed488819e03efc601cfa125ad7
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-07-30 06:10:51 -0400

    invoices start

--- routes/invoices.js
+++ routes/invoices.js
@@ -1,3 +1,30 @@
 const express = require("express");
 const ExpressError = require("../expressError");
 const db = require("../db");
+
+let router = new express.Router();
+
+router.get("/", async function (req, res, next) {
+  try {
+  } catch (err) {}
+});
+
+router.get("/", async function (req, res, next) {
+  try {
+  } catch (err) {}
+});
+
+router.post("/", async function (req, res, next) {
+  try {
+  } catch (err) {}
+});
+
+router.put("/", async function (req, res, next) {
+  try {
+  } catch (err) {}
+});
+
+router.delete("/", async function (req, res, next) {
+  try {
+  } catch (err) {}
+});
