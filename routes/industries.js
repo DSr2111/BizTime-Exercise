502176076db3f887114a391bb9abd31330a49d99
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-07-30 07:50:57 -0400

    route to add company

--- routes/industries.js
+++ routes/industries.js
@@ -3,3 +3,20 @@ const ExpressError = require("../expressError");
 const db = require("../db");
 
 let router = new express.router();
+
+router.post("/", async function (req, res, next) {
+  try {
+    let { industry } = req.body;
+
+    const result = await db.query(
+      `INSERT INTO industries (industry)
+        VALUES ($1)
+        RETURNING id, industry`,
+      [industry]
+    );
+
+    return res.status(201).json({ industry: result.rows[0] });
+  } catch (err) {
+    return next(err);
+  }
+});
